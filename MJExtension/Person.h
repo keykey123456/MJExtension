1c554536404c3eb2ffc47bcf3acd1aa798cd3152
海一长虹项目 <tuotuoyi@haiyichangxingnantongdeMac-mini.local>
2016-01-30 22:29:20 +0800

    添加Person类

---
+++ MJExtension/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  MJExtensionExample
+//
+//  Created by 海一长虹项目 on 16/1/30.
+//  Copyright (c) 2016年 小码哥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ MJExtension/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  MJExtensionExample
+//
+//  Created by 海一长虹项目 on 16/1/30.
+//  Copyright (c) 2016年 小码哥. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
